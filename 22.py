813245acb1d6ff6f86559ad8fef585ed7b954743
AlanHusovic <alanhusovic755@gmail.com>
2026-02-15 10:42:08 +0100

    Add roller coaster height check program

---
+++ 22.py
@@ -0,0 +1,7 @@
+print("Dobro dosli na rolerkoster!!!")
+visina = int(input("Koliko si visok u cm: "))
+
+if visina >= 120:
+    print("Mozes se voziti na rolerkoster!!")
+else:
+    print("Zao mi je moras porasti da bih se vozio na rolerkoster.")
